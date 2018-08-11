38b02dc80649960de20e8fde39ed910476cad129
ngducnhat <ngducnhat@gmail.com>
2018-08-11 17:57:43 +0700

    commit all

---
+++ Assets/Nexweron/Nexweron/MSKBridgeAVPLiveCamera/MSKBridgeAVPLiveCamera.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using Nexweron.Core.MSK;
+
+public class MSKBridgeAVPLiveCamera : MSKBridgeBase {
+	
+	public AVProLiveCamera avpLiveCamera;
+	public MSKController mskController;
+	
+	private void UpdateTexture(Texture texture) {
+		if (sourceTexture != texture) {
+			sourceTexture = texture;
+
+			if (mskController != null) {
+				mskController.SetSourceTexture(sourceTexture);
+			} else {
+				Debug.LogError("MSKBridgeAVPLiveCamera | mskController = null");
+			}
+		}
+	}
+	
+	private int _framesCounter = 0;
+
+	private void Update() {
+		if(avpLiveCamera != null) {
+			var framesCounter = avpLiveCamera.Device.FramesTotal;
+			if (framesCounter > 0) {
+				UpdateTexture(avpLiveCamera.OutputTexture);
+			}
+			if (_sourceTexture != null) {
+				if (_framesCounter != framesCounter) {
+					_framesCounter = framesCounter;
+
+					mskController.RenderOut(_texture);
+				}
+			}
+		} else {
+			Debug.LogError("MSKBridgeAVPLiveCamera | avpLiveCamera = null");
+		}
+	}
+}
---
+++ Assets/Scripts/Manager/ControllerManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+using UnityEngine.Video;
+
+public class ControllerManager : MonoBehaviour {
+    [SerializeField]private InputField videoInput;
+    private string videoInputPath;
+    [SerializeField] private InputField backgroundInput;
+    private string backgroundInputPath;
+    [SerializeField] private RawImage backgroundImage;
+    [SerializeField] private VideoPlayer videoPlayer;
+    [SerializeField] private RenderTexture videoPlayerTexture;
+    [SerializeField] private RawImage videoPlayerViewport;
+
+    private void Awake()
+    {
+        videoPlayerTexture.Release();
+        videoPlayerTexture.width = Screen.width;
+        videoPlayerTexture.height = Screen.height;
+    }
+    public void OnVideoInputClick()
+    {
+        videoInputPath = EditorUtility.OpenFilePanel("Choose your video", "", "");
+        videoInput.text = videoInputPath.Split('/').Last();
+    }
+
+    public void OnBackgroundInputClick()
+    {
+        backgroundInputPath = EditorUtility.OpenFilePanel("Choose your background", "", "");
+        backgroundInput.text = backgroundInputPath.Split('/').Last();
+        Texture2D tempTexture = (new WWW(backgroundInputPath)).texture;
+        if (tempTexture == null)
+        {
+            EditorUtility.DisplayDialog("Warning", "Please choose correct background image format!", "OK");
+        } else
+        {
+            backgroundImage.texture = tempTexture;
+        }
+    }
+
+    public void OnPlayBtnClick()
+    {
+        if (videoInput == null || videoInputPath == "" || videoInputPath == string.Empty)
+        {
+            return;
+        }
+        videoPlayer.url = videoInputPath;
+        videoPlayer.prepareCompleted += VideoPlay;
+        videoPlayer.Prepare();
+    }
+
+    public void OnStopBtnClick()
+    {
+        videoPlayer.Stop();
+        videoPlayerTexture.Release();
+        backgroundImage.color = Color.black;
+    }
+
+    public void VideoPlay(VideoPlayer player)
+    {
+        //float screenRatio = (float)Screen.width / (float)Screen.height;
+        //float videoRatio = (float)videoPlayer.texture.width / (float)videoPlayer.texture.height;
+        //videoPlayerViewport.rectTransform.sizeDelta = (screenRatio > videoRatio) ? new Vector2(Screen.height * videoRatio, Screen.height) : new Vector2(Screen.width, Screen.width / videoRatio);
+        backgroundImage.color = Color.white;
+        player.prepareCompleted -= VideoPlay;
+        player.Play();
+    }
+}
+
