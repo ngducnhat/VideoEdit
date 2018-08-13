3a051302e60742189d6d55283d929ed0cf935a10
ngducnhat <ngducnhat@gmail.com>
2018-08-13 22:32:47 +0700

    push missing files

---
+++ Assets/Scripts/Manager/Utils.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using DG.Tweening;
+using System.Reflection;
+
+public class Utils : MonoBehaviour {
+    public static Utils Instance;
+
+    [SerializeField] private TextMeshProUGUI alertText;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+    public void ShowAlert(string message)
+    {
+        if (alertText != null)
+        {
+            alertText.text = message;
+            alertText.GetComponent<CanvasGroup>().DOFade(1f, 0.5f).OnComplete(() => {
+                Invoke("HideAlert", 1f);
+            });
+        }
+    }
+
+    public void HideAlert()
+    {
+        if (alertText != null)
+        {
+            alertText.GetComponent<CanvasGroup>().DOFade(0f, 1f);
+        }
+    }
+}
