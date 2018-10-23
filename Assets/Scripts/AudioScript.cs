b796f10c5c23c71b7cadc0cdee911222e2bd614a
hongquang9 <quangnguyendt9@gmail.com>
2018-10-23 16:23:01 +0900

    Add audio object
    Quang add plugin to get audio from microphone. It need to connect microphone to computer and then it will automatically record and play on speaker

---
+++ Assets/Scripts/AudioScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioScript : MonoBehaviour
+{
+
+    public string microphone;
+    public AudioSource AudioSource;
+
+    // Use this for initialization
+    void Start()
+    {
+        AudioSource = GetComponent<AudioSource>();
+
+        //Get all available microphone
+        foreach (string device in Microphone.devices)
+        {
+            Debug.Log("Found microphone device: " + device);
+            if (microphone == null)
+            {
+                //set default mic to first mic found
+                microphone = device;
+            }
+        }
+        if (microphone == null) microphone = "Built-in Microphone";
+        UpdateMicrophone();
+    }
+
+    // Update is called once per frame
+    void UpdateMicrophone()
+    {
+        AudioSource.Stop();
+        AudioSource.clip = Microphone.Start(microphone, true, 10, 44100);
+        AudioSource.loop = true;
+
+        if (Microphone.IsRecording(microphone))
+        {
+            while (!(Microphone.GetPosition(microphone) > 0)) { }
+            AudioSource.Play();
+        }
+        else
+        {
+            Debug.Log(microphone + "does not work!");
+        }
+    }
+}
